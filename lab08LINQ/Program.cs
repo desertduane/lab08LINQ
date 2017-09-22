7a8817000dd574a30c96241787110b8ca020c004
duane rush <desertduane@yahoo.com>
2017-09-22 08:42:31 -0700

    Add project files.

---
+++ lab08LINQ/Program.cs
@@ -0,0 +1,76 @@
+﻿using QuickType;
+using System.Linq;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace lab08LINQ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fileName = "data.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"data\", fileName);
+            string jsonFile;
+
+            // Reading the Json
+            using (StreamReader sr = File.OpenText(path))
+            {
+                jsonFile = File.ReadAllText(path);
+              
+            }
+
+
+            
+            var data = GettingStarted.FromJson(jsonFile);
+
+            IEnumerable<Feature> allNeighborhoods = from n in data.Features
+                                                   where n.Properties.Neighborhood != null
+                                                   select n;
+
+            foreach (var n in data.Features)
+            {
+                Console.WriteLine(n.Properties.Neighborhood);
+            }
+            Console.Clear();
+
+            // removes the blanks
+
+            var noBlankNeighborhoods = allNeighborhoods.Where(n => n.Properties.Neighborhood != "");
+
+            foreach(var n in noBlankNeighborhoods)
+            {
+                Console.WriteLine(n.Properties.Neighborhood);
+            }
+            Console.Clear();
+
+            // removes the duplicate neighborhoods
+
+            var uniqueNeighborhoods = noBlankNeighborhoods.GroupBy(n => n.Properties.Neighborhood).Select(n => n.First());
+
+            foreach(var n in uniqueNeighborhoods)
+            {
+                Console.WriteLine(n.Properties.Neighborhood);
+            }
+            Console.Clear();
+            
+            // all of the above in one
+
+            var allInOne = data.Features.Where(n => n.Properties.Neighborhood != "").GroupBy(n => n.Properties.Neighborhood).Select(n => n.First());
+
+            foreach(var n in allInOne)
+            {
+                Console.WriteLine(n.Properties.Neighborhood);
+            }
+
+
+            Console.Read();
+        
+
+            
+
+            
+        }
+    }
+}
---
+++ lab08LINQ/README.md
@@ -0,0 +1,16 @@
+
+
+## ReadMe
+
+	- How long did it take you to complete this assignment?
+	about 4 hours.
+
+	- What did you struggle with? Why? How did you solve?
+	This assignment as a whole was a stuggle for me but thanks to the help and support of my fellow students
+	and self research I was able to get there. 
+
+	- What did you learn during this assignment?
+	How to bring in a Json file and Query for the data you want. 
+
+    - What resources did you utilize for this assignment?
+	microsoft docs and much help from other students.
